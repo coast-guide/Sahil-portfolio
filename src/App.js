9c8831adfa278273d087733d89a66b6de332f835
SK SAHIL <sksahil.contact@gmail.com>
2022-07-20 16:30:59 +0530

    Add Routes to Home, About, Skills, Resume, Portfolio and Contact page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import About from './containers/about/index.jsx';
+import Contact from './containers/contact/index';
+import Home from './containers/home/index';
+import Portfolio from './containers/portfolio/index';
+import Resume from './containers/resume/index';
+import Skills from './containers/skills/index';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      {/* particls js */}
+      {/* navbar component */}
+      {/* Main page */}
+      <Routes>
+        {/* Create all routes */}
+        <Route path='/' index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
