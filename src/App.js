479ca4a157fddb1d4704df8f7801cf887b5776f4
SK SAHIL <sksahil.contact@gmail.com>
2022-07-21 18:52:39 +0530

    Add wrapper around Navbar and main-content and styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './containers/home/index';
 import Portfolio from './containers/portfolio/index';
 import Resume from './containers/resume/index';
 import Skills from './containers/skills/index';
-import './App.css';
+import './App.scss';
 import Navbar from './components/navBar/index.jsx';
 import particlesConfig from './helpers/particlesConfig.js';
 
@@ -17,7 +17,6 @@ function App() {
   };
 
   const location = useLocation();
-  console.log(location);
   return (
     <div className='App'>
       {/* particls js */}
@@ -29,17 +28,20 @@ function App() {
         />
       )}
       {/* navbar component */}
-      <Navbar />
-      {/* Main page */}
-      <Routes>
-        {/* Create all routes */}
-        <Route path='/' index element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path='/resume' element={<Resume />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <div className='App__navbar-wrapper'>
+        <Navbar />
+      </div>
+      {/* Main page content*/}
+      <div className='App__main-content-wrapper'>
+        <Routes>
+          <Route path='/' index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
