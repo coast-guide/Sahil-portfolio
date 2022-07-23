963037e33ea7caaf573d17eadf8eb7cc58da8a2f
SK SAHIL <sksahil.contact@gmail.com>
2022-07-23 20:41:19 +0530

    Add Resume page.

---
+++ src/containers/resume/data.js
@@ -0,0 +1,11 @@
+export const data = {
+  experience: [],
+  education: [
+    {
+      title: 'B.E.',
+      subTitle: 'Jadavpur University',
+      description: 'Bachelor of Engineering in Information Technology',
+      date: '2019 - 2023',
+    },
+  ],
+};
